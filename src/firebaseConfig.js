d278186bb2beb21c9b174346379799ceb3235fe5
Juan Foncuberta <juan.foncuberta@gmail.com>
2024-06-04 23:06:34 +0200

    Adding signupUser to the store

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -15,15 +15,6 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const auth = getAuth()
-createUserWithEmailAndPassword(auth,email,password)
-  .then((userCredential) => {
-    const user =  userCredential.user;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  })
-
 
 export { auth }
 
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,15 +1,24 @@
 import { defineStore } from "pinia";
-
+import { auth } from "../firebaseConfig"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+``
 
 export const useUserStore = defineStore('userStore', {
   state:()=> ({
-      userData: 'pepe'
+      userData: null
   }),
   getters:  {
    
   },
   actions: {
-
+      async signupUser(email,password) {
+        try {
+           const { user } = await createUserWithEmailAndPassword(auth,email,password)
+           console.log(user)
+        } catch (error) {
+            console.log(`Error signup user ${error.message}}`)
+        }
+      }
   }
 
 })
