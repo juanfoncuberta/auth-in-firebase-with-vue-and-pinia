21ed1ccc7967fa4f8bba8735f7283b9fa9d47b31
Juan Foncuberta <juan.foncuberta@gmail.com>
2024-08-06 14:06:58 +0200

    Adding restaurant list

---
+++ src/components/restaurantsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Restaurants list</h1>
+  <p v-if="databaseStore.loadingDoc"></p>
+    <ul v-else>
+      <li v-for="item of databaseStore.documents" :key="item.id">
+          {{ item.name }} - {{ item.owner  }}
+      </li>
+    </ul>
+</template>
+
+<script setup>
+
+import { useDataBaseStore }  from '../stores/database'
+
+const databaseStore = useDataBaseStore()
+databaseStore.getRestaurants()
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
     <h1>Home</h1>
-    <p v-if="databaseStore.loadingDoc"></p>
-    <ul v-else>
-      <li v-for="item of databaseStore.documents" :key="item.id">
-          {{ item.name }} - {{ item.owner  }}
-      </li>
-    </ul>
+    <restaurantsList />
    {{ userStore.userData }}
-
 </template>
 
 <script setup>
 import { useUserStore } from '../stores/userStore'
-import { useDataBaseStore }  from '../stores/database'
+import restaurantsList from '../components/restaurantsList.vue';
 
 const userStore = useUserStore()
-const databaseStore = useDataBaseStore()
-databaseStore.getRestaurants()
 </script>
